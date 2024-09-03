0d8841951fc4b1f6553db46c42f448d3f98f278d
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2024-09-03 09:19:47 +0200

    reworked GO code. using io.Copy. muss less code

--- go/hashp.go
+++ go/hashp.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha256"
 	"encoding/hex"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"io/fs"
+	"log"
 	"os"
 	"path"
 	"path/filepath"
@@ -28,18 +29,38 @@ type ToHash struct {
 	size int64
 }
 
-type HashData struct {
-	len int
-	buf []byte
+type MuxWriter struct {
+	mux    *sync.Mutex
+	writer *bufio.Writer
+	fp     *os.File
 }
 
-type HashResult struct {
-	filename string
-	filesize int64
-	hash     []byte
+func NewMuxWriter(filename string, bufsize int) *MuxWriter {
+
+	fp, err := os.Create(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return &MuxWriter{
+		fp:     fp,
+		mux:    &sync.Mutex{},
+		writer: bufio.NewWriterSize(fp, 64*1024),
+	}
 }
 
-func printErr(api string, err error) {
+func (mw *MuxWriter) WriteString(s string) (int, error) {
+	mw.mux.Lock()
+	defer mw.mux.Unlock()
+	return mw.writer.WriteString(s)
+}
+
+func (mw *MuxWriter) Close() {
+	mw.writer.Flush()
+	mw.fp.Close()
+}
+
+func printErr(err error) {
 	fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 }
 
@@ -56,23 +77,28 @@ func ByteCountIEC(b uint64) string {
 	return fmt.Sprintf("%.2f %ciB",
 		float64(b)/float64(div), "KMGTPE"[exp])
 }
-func printStats(stats *Stats, statsLast *Stats, pauseSecs uint) {
+func printStats(stats *Stats, pauseSecs uint) {
 
-	filesRead := atomic.LoadUint64(&stats.filesRead)
-	bytesRead := atomic.LoadUint64(&stats.bytesRead)
+	statsLast := Stats{}
 
-	bytesReadDiff := bytesRead - statsLast.bytesRead
-	filesReadDiff := filesRead - statsLast.filesRead
+	for {
+		time.Sleep(time.Duration(pauseSecs) * time.Second)
+		filesRead := atomic.LoadUint64(&stats.filesRead)
+		bytesRead := atomic.LoadUint64(&stats.bytesRead)
 
-	fmt.Printf("files: %12d %10s | files/s: %6d %4d MB/s | err: %d\n",
-		filesRead,
-		ByteCountIEC(bytesRead),
-		filesReadDiff/uint64(pauseSecs),
-		bytesReadDiff/uint64(pauseSecs)/1024/1024,
-		atomic.LoadUint64(&stats.errors))
+		bytesReadDiff := bytesRead - statsLast.bytesRead
+		filesReadDiff := filesRead - statsLast.filesRead
 
-	statsLast.bytesRead = bytesRead
-	statsLast.filesRead = filesRead
+		fmt.Printf("files: %12d %10s | files/s: %6d %4d MB/s | err: %d\n",
+			filesRead,
+			ByteCountIEC(bytesRead),
+			filesReadDiff/uint64(pauseSecs),
+			bytesReadDiff/uint64(pauseSecs)/1024/1024,
+			atomic.LoadUint64(&stats.errors))
+
+		statsLast.bytesRead = bytesRead
+		statsLast.filesRead = filesRead
+	}
 }
 
 func enumerate(directoryname string, files chan<- ToHash, errFunc func(error)) {
@@ -98,101 +124,28 @@ func enumerate(directoryname string, files chan<- ToHash, errFunc func(error)) {
 	}
 }
 
-func hashWriter(filename string, rootDir string, hashes <-chan HashResult, wg *sync.WaitGroup) {
+func readAndHashFiles(files <-chan ToHash, lenRootDir int, stats *Stats, hashWriter *MuxWriter, errFunc func(error), wg *sync.WaitGroup) {
 	defer wg.Done()
-
-	lenRootDir := len(rootDir)
-
-	fp, err := os.Create(filename)
-	if err != nil {
-		panic(fmt.Sprintf("could not create result file for hashes. err: %s", err))
-	} else {
-		defer fp.Close()
-
-		for hash := range hashes {
-			relativeFilename := hash.filename[lenRootDir+1:]
-			fp.WriteString(fmt.Sprintf("%s %12d %s\n", hex.EncodeToString(hash.hash), hash.filesize, relativeFilename))
-		}
-	}
-}
-
-func hasher(files <-chan ToHash, filedata <-chan HashData, hashes chan<- HashResult, hasherRunning *int32) {
-
-	defer func() {
-		if atomic.AddInt32(hasherRunning, -1) == 0 {
-			// I'm the last hasher.
-			// close the single channel of HashResults
-			close(hashes)
-		}
-	}()
-
-	h := sha256.New()
+	hash := sha256.New()
 
 	for file := range files {
-		h.Reset()
-		for data := range filedata {
-			if data.len == -1 {
-				// error readinf the file
-				// don't send a result for this file
-				break
-			} else if data.len == 0 {
-				hashes <- HashResult{file.path, file.size, h.Sum(nil)}
-				break
-			} else {
-				h.Write(data.buf)
-			}
-		}
-	}
-}
-
-func sendFileToHasher(file ToHash, hasherFiles chan<- ToHash, hasherData chan<- HashData, bufs [][]byte, stats *Stats, errFunc func(error)) {
-	fp, err := os.Open(file.path)
-	if err != nil {
-		errFunc(err)
-	} else {
-		defer fp.Close()
-
-		hasherFiles <- file
-
-		bufIdx := 0
-		for {
-			bufIdx = 1 - bufIdx
-			buf := bufs[bufIdx]
-			numberRead, err := fp.Read(buf[:])
-			if err != nil && !errors.Is(err, io.EOF) {
-				hasherData <- HashData{-1, nil} // signal read error to hasher
+		fp, err := os.Open(file.path)
+		if err != nil {
+			errFunc(err)
+		} else {
+			hash.Reset()
+			written, err := io.Copy(hash, fp)
+			fp.Close()
+			if err != nil {
 				errFunc(err)
-				break
 			} else {
-				atomic.AddUint64(&stats.bytesRead, uint64(numberRead))
-				hasherData <- HashData{numberRead, buf[:numberRead]}
-
-				if numberRead == 0 {
-					break
-				}
+				atomic.AddUint64(&stats.filesRead, 1)
+				atomic.AddUint64(&stats.bytesRead, uint64(written))
+				relativeFilename := file.path[lenRootDir+1:]
+				hashWriter.WriteString(fmt.Sprintf("%s %12d %s\n", hex.EncodeToString(hash.Sum(nil)), file.size, relativeFilename))
 			}
 		}
-		atomic.AddUint64(&stats.filesRead, 1)
 	}
-}
-
-func readFilesSendToHasher(files <-chan ToHash, hashes chan<- HashResult, bufsize int, stats *Stats, hasherRunning *int32, errFunc func(error)) {
-
-	hasherFiles := make(chan ToHash)
-	hasherData := make(chan HashData) // a channel with ONLY 1 item possible!!! due to "double buffering" with read
-
-	atomic.AddInt32(hasherRunning, 1)
-	go hasher(hasherFiles, hasherData, hashes, hasherRunning)
-
-	bufs := make([][]byte, 2)
-	bufs[0] = make([]byte, bufsize)
-	bufs[1] = make([]byte, bufsize)
-
-	for file := range files {
-		sendFileToHasher(file, hasherFiles, hasherData, bufs, stats, errFunc)
-	}
-	close(hasherData)
-	close(hasherFiles)
 }
 
 func getRootDir(pathToHash string, pathToHashStat fs.FileInfo) string {
@@ -207,16 +160,10 @@ func getRootDir(pathToHash string, pathToHashStat fs.FileInfo) string {
 	}
 }
 
-func createErrorFunc(filename string, errCounter *uint64) (*os.File, func(error)) {
-
-	fp, err := os.Create(filename)
-	if err != nil {
-		panic(fmt.Sprintf("could not create file for errors. %s", filename))
-	}
-
-	return fp, func(err error) {
+func createErrorFunc(writer *MuxWriter, errCounter *uint64) func(error) {
+	return func(err error) {
 		atomic.AddUint64(errCounter, 1)
-		fmt.Fprintf(fp, "%s\n", err.Error())
+		writer.WriteString(fmt.Sprintf("%s\n", err.Error()))
 	}
 }
 
@@ -225,8 +172,8 @@ func main() {
 	var workers int
 	var bufsize int
 	defaultWorker := runtime.NumCPU()
-	flag.IntVar(&workers, "w", defaultWorker, "number of workers (Number CPUs)")
-	flag.IntVar(&bufsize, "b", 4096, "buffersize read")
+	flag.IntVar(&workers, "w", defaultWorker, "number of workers (number CPUs)")
+	flag.IntVar(&bufsize, "b", 64*1024, "buffersize")
 	flag.Parse()
 
 	if len(flag.Args()) != 1 {
@@ -237,37 +184,33 @@ func main() {
 
 	pathToHash, err := filepath.Abs(flag.Arg(0))
 	if err != nil {
-		printErr("filepath.Abs", err)
-		os.Exit(8)
+		log.Fatal(err)
 	}
 
 	pathToHashStat, err := os.Stat(pathToHash)
 	if err != nil {
-		printErr("stat", err)
-		os.Exit(8)
+		log.Fatal(err)
 	}
 
 	var stats = Stats{}
-	var statsLast = Stats{}
-
-	errFp, errFunc := createErrorFunc("./errors.txt", &stats.errors)
-	defer errFp.Close()
-
-	// channel to the writer of hashes
-	hashes := make(chan HashResult, 128)
 
 	rootDir := getRootDir(pathToHash, pathToHashStat)
-	var wgWriter sync.WaitGroup
-	wgWriter.Add(1)
-	go hashWriter("./hashes.txt", rootDir, hashes, &wgWriter)
 
 	// channel from enumerate to read files
-	var MAX_ENUMERATE = defaultWorker * 8
+	var MAX_ENUMERATE = 1024
 	files := make(chan ToHash, MAX_ENUMERATE)
 
-	var hasherRunning int32 = 0
+	errWriter := NewMuxWriter("errors.txt", 64*1024)
+	hashWriter := NewMuxWriter("hashes.txt", 128*1024)
+	defer errWriter.Close()
+	defer hashWriter.Close()
+
+	errFunc := createErrorFunc(errWriter, &stats.errors)
+
+	var wgHasher sync.WaitGroup
 	for i := 0; i < workers; i++ {
-		go readFilesSendToHasher(files, hashes, bufsize, &stats, &hasherRunning, errFunc)
+		wgHasher.Add(1)
+		go readAndHashFiles(files, len(rootDir), &stats, hashWriter, errFunc, &wgHasher)
 	}
 
 	if pathToHashStat.IsDir() {
@@ -277,22 +220,7 @@ func main() {
 		close(files)
 	}
 
-	finished := make(chan struct{})
-	go func() {
-		wgWriter.Wait()
-		close(finished)
-	}()
+	go printStats(&stats, 2)
 
-	var statsPauseSecs uint = 2
-loop:
-
-	for {
-		select {
-		case <-finished:
-			printStats(&stats, &statsLast, statsPauseSecs)
-			break loop
-		case <-time.After(time.Duration(statsPauseSecs) * time.Second):
-			printStats(&stats, &statsLast, statsPauseSecs)
-		}
-	}
+	wgHasher.Wait()
 }
